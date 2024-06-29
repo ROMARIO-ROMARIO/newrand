f7eac3569a24e1e516c41cd48182f1b105353a30
ROMARIO-ROMARIO <ro.mar.io.roma@mail.ru>
2024-06-29 14:02:14 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,29 +1,39 @@
 const express = require('express');
+const axios = require('axios');
 const app = express();
 
-// Используем переменную окружения PORT или, если она не задана, порт 4000
 const PORT = process.env.PORT || 4000;
 
-// Подключение CORS middleware
 app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*"); // В продуктивной среде замените "*" на точные домены, например "https://yourdomain.com"
+    res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
 
-// Обработка корневого запроса
 app.get('/', (req, res) => {
-    res.send('<h1>Добро пожаловать на сервер 2Генерации случайных чисел!</h1><p>Используйте /random для получения случайного числа.</p>');
+    res.send('<h1>Добро пожаловать на сервер генерации случайных чисел!</h1><p>Используйте /random для генерации числа.</p>');
 });
 
-// Обработка запросов на получение случайного числа
-app.get('/random', (req, res) => {
-      res.set('Cache-Control', 'no-store');  // Запрещаем кэширование данного ответа
-      const randomValue = Math.floor(Math.random() * 37); // Генерация числа от 0 до 36 включительно
-      res.json({ value: randomValue });
-  });
+app.get('/random', async (req, res) => {
+    try {
+        // Получение текущего времени с World Time API
+        const timeResponse = await axios.get('http://worldtimeapi.org/api/timezone/Etc/UTC');
+        const now = new Date(timeResponse.data.utc_datetime);
+        const mm = String(now.getUTCMinutes()).padStart(2, '0');
+        const ss = String(now.getUTCSeconds()).padStart(2, '0');
+        const hh = String(now.getUTCHours()).padStart(2, '0');
+        const dd = String(now.getUTCDate()).padStart(2, '0');
+        const yyyy = now.getUTCFullYear();
+        const rr = Math.floor(Math.random() * 37); // Случайное число от 0 до 36
 
-// Запуск сервера
+        const formattedOutput = `${mm}${ss}${hh}${dd}${yyyy}${rr}`;
+        res.json({ value: formattedOutput });
+    } catch (error) {
+        console.error('Ошибка при получении данных с World Time API:', error);
+        res.status(500).send('Ошибка при получении данных с World Time API');
+    }
+});
+
 app.listen(PORT, () => {
-    console.log(`Сервер запущen on порту ${PORT}. Перейдите на http://localhost:${PORT}/ для начала работы.`);
+    console.log(`Сервер запущен на порту ${PORT}. Перейдите на http://localhost:${PORT}/ для начала работы.`);
 });
